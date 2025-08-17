6e6e3b40838f98370591c833021d38d7c20040d3
Mridul Tailor <mtailor729@gmail.com>
2025-08-16 18:39:00 -0700

    refactor: include tailwind config

--- build.sh
+++ build.sh
@@ -2,12 +2,18 @@
 # exit on error
 set -o errexit
 
-# Install pipenv if not available
-pip install pipenv
-
 # Install dependencies
 pipenv install --deploy
 
-# Collect static files and run migrations
+# Install Node.js dependencies and build Tailwind CSS
+pipenv run python manage.py tailwind install --no-input
+pipenv run python manage.py tailwind build --no-input
+
+# Collect static files (this will now include compiled Tailwind CSS)
 pipenv run python manage.py collectstatic --no-input
+
+# Run database migrations
 pipenv run python manage.py migrate
+
+# Load CSV data
+pipenv run python manage.py load_csv_data
